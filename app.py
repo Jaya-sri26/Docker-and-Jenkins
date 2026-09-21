cd776c07f79a9ba1c4ada4f69b954f91f194ecd2
Jayasri Tammineedi <jayasritammineedi57@gmail.com>
2026-09-21 10:03:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def register():
     return render_template('successful.html',name=name,year=year)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5001,debug=True)
